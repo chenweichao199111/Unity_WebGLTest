6891d0e856fc9173df5848045d25db6f5c51c6e1
chenweichao199111 <1027818327@qq.com>
2023-05-07 15:58:08 +0800

    上传文件

---
+++ Assets/Demo/DevicePerformanceUtil.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+namespace Logic
+{
+    public static class DevicePerformanceUtil
+    {
+        /// <summary>
+        /// 获取设备性能评级
+        /// </summary>
+        /// <returns>性能评级</returns>
+        public static DevicePerformanceLevel GetDevicePerformanceLevel()
+        {
+            if (SystemInfo.graphicsDeviceVendorID == 32902)
+            {
+                Debug.Log("当前设备显卡是集成显卡");
+                //集显
+                return DevicePerformanceLevel.Low;
+            }
+            else //NVIDIA系列显卡(N卡)和AMD系列显卡
+            {
+                Debug.Log("CPU核心数:" + SystemInfo.processorCount);
+                //根据目前硬件配置三个平台设置了不一样的评判标准(仅个人意见)
+#if UNITY_EDITOR || UNITY_STANDALONE_WIN || UNITY_WEBGL
+                if (SystemInfo.processorCount <= 2)
+#elif UNITY_STANDALONE_OSX || UNITY_IPHONE
+            if (SystemInfo.processorCount < 2)
+#elif UNITY_ANDROID
+            if (SystemInfo.processorCount <= 4)
+#endif
+                {
+                    //CPU核心数<=2判定为低端
+                    return DevicePerformanceLevel.Low;
+                }
+                else
+                {
+                    //显存
+                    int graphicsMemorySize = SystemInfo.graphicsMemorySize;
+                    Debug.Log("显存:" + graphicsMemorySize);
+                    //内存
+                    int systemMemorySize = SystemInfo.systemMemorySize;
+                    Debug.Log("内存:" + systemMemorySize);
+#if UNITY_EDITOR || UNITY_STANDALONE_WIN || UNITY_WEBGL
+                    if (graphicsMemorySize >= 4000 && systemMemorySize >= 8000)
+                        return DevicePerformanceLevel.High;
+                    else if (graphicsMemorySize >= 2000 && systemMemorySize >= 4000)
+                        return DevicePerformanceLevel.Mid;
+                    else
+                        return DevicePerformanceLevel.Low;
+#elif UNITY_STANDALONE_OSX || UNITY_IPHONE
+            if (graphicsMemorySize >= 4000 && systemMemorySize >= 8000)
+                    return DevicePerformanceLevel.High;
+                else if (graphicsMemorySize >= 2000 && systemMemorySize >= 4000)
+                    return DevicePerformanceLevel.Mid;
+                else
+                    return DevicePerformanceLevel.Low;
+#elif UNITY_ANDROID
+            if (graphicsMemorySize >= 6000 && systemMemorySize >= 8000)
+                    return DevicePerformanceLevel.High;
+                else if (graphicsMemorySize >= 2000 && systemMemorySize >= 4000)
+                    return DevicePerformanceLevel.Mid;
+                else
+                    return DevicePerformanceLevel.Low;
+#endif
+                }
+            }
+        }
+    }
+    public enum DevicePerformanceLevel
+    {
+        Low,
+        Mid,
+        High
+    }
+}
---
+++ Assets/Demo/TestDevice.cs
@@ -0,0 +1,56 @@
+﻿using Logic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestDevice : MonoBehaviour
+{
+    public TMP_Text textMeshPro;
+    public Button getDeviceBtn;
+
+    void Start()
+    {
+        DevicePerformanceLevel dpl = DevicePerformanceUtil.GetDevicePerformanceLevel();
+        if (dpl == DevicePerformanceLevel.High)
+        {
+            Debug.Log("当前设备是高性能设备");
+            QualitySettings.SetQualityLevel(5);
+        }
+        else if (dpl == DevicePerformanceLevel.Mid)
+        {
+            Debug.Log("当前设备是中性能设备");
+            QualitySettings.SetQualityLevel(2);
+        }
+        else 
+        {
+            Debug.Log("当前设备是低性能设备");
+            int level = QualitySettings.GetQualityLevel();
+            Debug.Log("质量等级由" + level + "调成0");
+            QualitySettings.SetQualityLevel(0, true);
+        }
+
+        #region 显示部分
+        if (dpl == DevicePerformanceLevel.High)
+        {
+            textMeshPro.text = string.Format("显卡存储：{0}MB\r\nCPU核心：{1}\r\n内存：{2}MB\r\n当前设备是高性能设备",
+                SystemInfo.graphicsMemorySize, SystemInfo.processorCount, SystemInfo.systemMemorySize);
+        }
+        else if (dpl == DevicePerformanceLevel.Mid)
+        {
+            textMeshPro.text = string.Format("显卡存储：{0}MB\r\nCPU核心：{1}\r\n内存：{2}MB\r\n当前设备是中性能设备",
+                SystemInfo.graphicsMemorySize, SystemInfo.processorCount, SystemInfo.systemMemorySize);
+        }
+        else
+        {
+            textMeshPro.text = string.Format("显卡存储：{0}MB\r\nCPU核心：{1}\r\n内存：{2}MB\r\n当前设备是低性能设备",
+                SystemInfo.graphicsMemorySize, SystemInfo.processorCount, SystemInfo.systemMemorySize);
+        }
+        getDeviceBtn.onClick.AddListener(OnClickGetDeviceBtn);
+        #endregion
+    }
+
+    private void OnClickGetDeviceBtn()
+    {
+        GUIUtility.systemCopyBuffer = textMeshPro.text;
+    }
+}
